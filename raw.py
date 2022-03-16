949fea520cade477739f9a5f754a7c7bf3c17955
irfan1965 <100187110+irfan1965@users.noreply.github.com>
2022-03-16 15:07:18 +0530

    Add files via upload

--- raw.py
+++ raw.py
@@ -11,10 +11,12 @@ c=0
 x=[]
 for i in range(1,len(g)):
   x.append(g[i][0])
-print(x)
+#print(x)
 temp={}
+td={}
 for i in x:
   temp[i]=[]
+  td[i]=[]
 u=[]
 for i,j in dic.items():
   print(i,j)
@@ -26,15 +28,15 @@ class Vehicle:
         self.Number=Number
 class bike(Vehicle):
     def __init__(self,Name,Number):
-        print(Name,Number)
+        #print(Name,Number)
         Vehicle.__init__(self,Name,Number)
 class car(Vehicle):
     def __init__(self,Name,Number):
-        print(Name,Number)
+        #print(Name,Number)
         Vehicle.__init__(self,Name,Number)
 class bus(Vehicle):
     def __init__(self,Name,Number):
-        print(Name,Number)
+        #print(Name,Number)
         Vehicle.__init__(self,Name,Number)
 f=1
 while( f==1):
@@ -62,14 +64,18 @@ while( f==1):
     for v in range(sp,ep):
       if j[v]=="free" :
         temp[i].append(v)
-        print("your token number" ,str(i)+str(v))
+        td[i].append([name,number])
+        print(td)
+        print("|------------------------------------|")
+        print("|your token number   :      |1A12{}{}| |".format(str(i),str(v)))
+        print("|------------------------------------|")
         c=1
         j[v]="occupied"
         break
     if(c==1):
       break
   
-  print(temp)
+  #print(temp)
   #print("your token number")
   for i ,j in temp.items():
     if len(j)>=1:
@@ -82,22 +88,35 @@ while( f==1):
         try:
           if temp[n[0]][temp[n[0]].index(int(n[1]))] in temp[n[0]]:
             m=float(input("enter how many hours"))
+            print()
+            print()
+            print("YOUR PARKING DETAILS --->")
             if m<=1 and m>0 :
+              print("|------------------------------------|")
               print("20 rupees only")
             elif m<=10:
               m=m*10
-              print("your charges will be {} ".format(m))
+              print("|------------------------------------|")
+              print("|your charges will be :     {}     ".format(m))
             else: 
               m=m*5
-              print("your charges will be {} ".format(m))
+              print("|------------------------------------|")
+              print("|your charges will be :     {}     ".format(m))
+
             dic[n[0]][int(n[1])]="free"
             #print(temp["A"].index(int(n[1])),"index")
-            print(td)
-            print(temp)
+            #print(td)
+            #print(temp)
+            print("|your car name    :     {}           ".format( td[n[0]][temp[n[0]].index(int(n[1]))][0]))
+            print("|your car number   :     {}          ".format( td[n[0]][temp[n[0]].index(int(n[1]))][1]))
+            print("|------------------------------------|")
+            print()
+            print()
+
             td[n[0]].remove(td[n[0]][temp[n[0]].index(int(n[1]))])
             temp[n[0]].remove(temp[n[0]].index(int(n[1])))
-            print(temp)
-            print(n[0],n[1])      
+            #print(temp)
+            #print(n[0],n[1])      
         except Exception :
             print("enter details correctly")
         
